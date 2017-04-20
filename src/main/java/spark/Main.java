5074bd698237303060a64d9057b06b53a5e818a8
mrryko <adrianhealeytheliar@gmail.com>
2017-04-20 11:02:06 +0300

    Update Main.java

--- src/main/java/spark/Main.java
+++ src/main/java/spark/Main.java
@@ -9,17 +9,18 @@ import org.codehaus.jettison.json.JSONException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+        Set<String> topics = Collections.singleton("sspark");
+        
         final String appName = "kafka-consumer";
         final String master = "local[*]";
         final String zookeper = "localhost:9092";
-        Set<String> topics = Collections.singleton("sspark");
-
         final String cassandraTable = "testSpark";
         final String cassandraKeySpace = "demoCassandra";
 
         /*configure cassandra's table and keyspace which we had to create before*/
         SparkCassandraWriter.configure(cassandraTable, cassandraKeySpace);
+        
         /* create new spark application and start it*/
         try {
             SparkKafkaReader cw = new SparkKafkaReader(appName, master);
